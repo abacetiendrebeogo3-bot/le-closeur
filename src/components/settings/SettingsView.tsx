9a2385d9419d7d277fba9eae4b4b51dadb5531f2
Tiedrebeogo Wilfried <wilfried@moncloseur.com>
2026-09-02 08:59:09 +0000

    fix_revert_invalid_fb_login_extras_permission_error

--- src/components/settings/SettingsView.tsx
+++ src/components/settings/SettingsView.tsx
@@ -247,8 +247,6 @@ export const SettingsView: React.FC<SettingsViewProps> = ({ triggerToast, ownerN
           override_default_response_type: true,
           extras: {
             setup: {},
-            featureType: "whatsapp_business_app_onboarding",
-            sessionInfoVersion: "3",
           },
         }
       );
@@ -334,8 +332,6 @@ export const SettingsView: React.FC<SettingsViewProps> = ({ triggerToast, ownerN
           override_default_response_type: true,
           extras: {
             setup: {},
-            featureType: "whatsapp_business_app_onboarding",
-            sessionInfoVersion: "3",
           },
         }
       );
